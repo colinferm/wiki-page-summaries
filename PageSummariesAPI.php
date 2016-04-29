650dd59101cbf913503b4f00c2c2649bdd1d4878
Colin <colinferm@users.noreply.github.com>
2016-04-29 18:45:31 -0400

    really removing comments

--- PageSummariesAPI.php
+++ PageSummariesAPI.php
@@ -99,20 +99,19 @@ class PageSummariesAPI extends ApiBase {
 }
 
 function addSummaryInfo( &$module ) {
-	wfDebugLog('addsummaryInfo', 'SummaryAPIExtension Api Extension loaded...');
+	//wfDebugLog('addsummaryInfo', 'SummaryAPIExtension Api Extension loaded...');
 	if(!($module instanceof ApiParse)) return true;
 
 	$result = $module->getResult();
 	$data = $result->getData();
 	
-	//wfDebugLog('addsummaryInfo', 'SummaryAPIExtension Data reads: '.$data['parse']['text']['*']);
 	if (!isset($data['parse']['text']['*'])) return true;
 	
 	$body = $data['parse']['text']['*'];
 	$matches = Array();
 	$c = preg_match_all('/<p>(.+?)<\/p>/is', $body, $matches);
 	$description;
-	wfDebugLog('addsummaryInfo', 'Number of matches: '.$c);
+	//wfDebugLog('addsummaryInfo', 'Number of matches: '.$c);
 	for ($i = 0; $i < count($matches[0]); $i++) {
 		$description = trim(preg_replace('/\"/', '\'', strip_tags($matches[0][$i])));
 		if (strlen($description)) break;
